0c58fd36166e6944409368c4dd8766ebfefd5aaf
Stanislas Chevallier <contact@stanislaschevallier.fr>
2021-10-31 12:41:34 +0100

    Update TempURL to use the provided filename if available and the original creation and modification dates

--- Groupir/Models/TempURL.swift
+++ Groupir/Models/TempURL.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+/*
+ @abstract This class is useful to create copies of items in the iOS gallery and exposing them to QLPreviewController and UIActivityViewController
+ without having sandbox issues. On APFS copies are basically free (in time and storage space) so this is an ideal situation
+ */
 class TempURL {
     // MARK: Static
     private static let baseFolder = FileManager.default
@@ -24,16 +28,33 @@ class TempURL {
     }
     
     // MARK: Init
-    init?(originalURL: URL) {
+    init?(originalURL: URL, filename: String) {
         self.originalURL = originalURL
         
         do {
-            url = TempURL.baseFolder
+            var url = TempURL.baseFolder
                 .appendingPathComponent(originalURL.deletingLastPathComponent().lastPathComponent, isDirectory: true)
-                .appendingPathComponent(originalURL.lastPathComponent)
+                .appendingPathComponent(filename)
 
+            var count = 0
+            while FileManager.default.fileExists(atPath: url.path) {
+                count += 1
+                url = url.deletingLastPathComponent()
+                    .appendingPathComponent((filename as NSString).deletingPathExtension + "-\(count)")
+                    .appendingPathExtension((filename as NSString).pathExtension)
+            }
+            self.url = url
+            
             try FileManager.default.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true, attributes: [:])
             try FileManager.default.copyItem(at: originalURL, to: url)
+            
+            let attributes = try FileManager.default.attributesOfItem(atPath: originalURL.path)
+            try [FileAttributeKey.creationDate, .modificationDate].forEach { key in
+                // useful when AirDrop-ing
+                if let value = attributes[key] {
+                    try FileManager.default.setAttributes([key: value], ofItemAtPath: url.path)
+                }
+            }
         }
         catch {
             Logger.e(.tempURL, "Couldn't create temp copy of \(originalURL): \(error)")
@@ -56,7 +77,7 @@ class TempURL {
 }
 
 extension URL {
-    var tempURL: TempURL? {
-        return TempURL(originalURL: self)
+    func tempURL(filename: String? = nil) -> TempURL? {
+        return TempURL(originalURL: self, filename: filename ?? self.lastPathComponent)
     }
 }
