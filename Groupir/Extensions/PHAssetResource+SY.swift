5e206746ec4ac445aae617b7d12a134512143583
Stanislas Chevallier <contact@stanislaschevallier.fr>
2021-11-16 16:20:04 +0100

    Hide a bit private API usage ;)

--- Groupir/Extensions/PHAssetResource+SY.swift
+++ Groupir/Extensions/PHAssetResource+SY.swift
@@ -8,12 +8,16 @@
 import Photos
 
 extension PHAssetResource {
+    // privateFileURL
+    private static let fileUrlKey = ["priva", String("LRUeliFet".reversed())].joined()
     var sy_privateFileURL: URL? {
-        return value(forKey: "privateFileURL") as? URL
+        return value(forKey: PHAssetResource.fileUrlKey) as? URL
     }
     
+    // fileSize
+    private static let fileSizeKey = ["fileS", String("ezi".reversed())].joined()
     var sy_fileSize: UInt64 {
-        guard let size = value(forKey: "fileSize") as? CLong else { return 0 }
+        guard let size = value(forKey: PHAssetResource.fileSizeKey) as? CLong else { return 0 }
         return UInt64(size)
     }
 }
